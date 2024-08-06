4d1bd523dc2f13f56e79c57c89607b2db3489936
douglas.araujo <douglas.araujo@artesanalinvestimentos.com.br>
2024-08-06 14:10:21 -0300

    feat: ex06

--- src/br/com/alura/screenmatch/exercises/principal/ex05.java
+++ src/br/com/alura/screenmatch/exercises/principal/ex05.java
@@ -2,12 +2,15 @@ package br.com.alura.screenmatch.exercises.principal;
 
 import br.com.alura.screenmatch.exercises.model.Titulo;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import java.util.Date;
 
 public class ex05 {
     public static void main(String[] args) {
-        Gson gson = new Gson();
+        Gson gson = new GsonBuilder()
+                .setPrettyPrinting()
+                .create();
         Titulo titulo = new Titulo("Matrix", new Date(), 120);
         String json = gson.toJson(titulo);
         System.out.println(json);
